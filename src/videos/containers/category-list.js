4ec04fd69e26ef1d2e5fd0e902482d33c396ba67
Leonardo Omaña <leonardoomana@Leonardos-MacBook-Pro.local>
2018-11-13 22:26:02 -0500

    Horizontal separator

--- src/videos/containers/category-list.js
+++ src/videos/containers/category-list.js
@@ -1,27 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
-  View,
   FlatList
-} from 'react-native';
-import Empty from '../components/empty';
-import Separator from '../components/vertical-separator';
-import Suggestion from '../components/suggestion';
+} from 'react-native'
+import Empty from '../components/empty'
+import Separator from '../../sections/components/horizontal-separator'
+import Suggestion from '../components/suggestion'
+import Layout from '../components/category-list-layout'
 
 export default class CategoryList extends Component {
   keyExtractor = item => item.id.toString()
   renderEmtpy = () => <Empty text="No hay sugerencias :(" />
   itemSeparator = () => <Separator />
-  renderItem = ({item}) => <Suggestion {...item} />
+  renderItem = ({ item }) => <Suggestion {...item} />
   render() {
     return (
-      <FlatList
-        horizontal
-        keyExtractor={this.keyExtractor}
-        data={this.props.list}
-        ListEmptyComponent={this.renderEmtpy}
-        ItemSeparatorComponent={this.itemSeparator}
-        renderItem={this.renderItem}
-      />
+      <Layout
+        title="Categorías"
+      >
+        <FlatList
+          horizontal
+          keyExtractor={this.keyExtractor}
+          data={this.props.list}
+          ListEmptyComponent={this.renderEmtpy}
+          ItemSeparatorComponent={this.itemSeparator}
+          renderItem={this.renderItem}
+        />
+      </Layout>
     )
   }
 }
