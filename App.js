5bd3b96e21d44926c69e6f5272503f4b90e92e39
Leonardo Omaña <leonardoomana@Leonardos-MacBook-Pro.local>
2018-11-21 21:38:46 -0500

    Charge component

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import Player from './src/player/containers/player'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './store'
+import Loading from './src/sections/components/loading'
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -47,7 +48,7 @@ export default class App extends Component<Props> {
         store={store}
       >
         <PersistGate
-          loading={<Text>Cargando...</Text>}
+          loading={<Loading />}
           persistor={persistor}
         >
           <Home>
