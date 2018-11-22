8b6ee7da8778641568ba6096f773b6776ca5e3e3
Leonardo Omaña <leonardoomana@Leonardos-MacBook-Pro.local>
2018-11-21 21:54:10 -0500

    Refactoring componets

--- App.js
+++ App.js
@@ -1,47 +1,12 @@
-import React, { Component } from 'react';
-import {
-  Text,
-  View
-} from 'react-native';
-import Home from './src/screens/containers/home';
-import Header from './src/sections/components/header'
-import API from './utils/api'
-import SuggestionList from './src/videos/containers/suggestion-list'
-import CategoryList from './src/videos/containers/category-list'
-import Player from './src/player/containers/player'
+import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './store'
 import Loading from './src/sections/components/loading'
+import AppLayout from './src/app'
 
 type Props = {};
 export default class App extends Component<Props> {
-  state = {
-    // suggestionList: [],
-    // categoryList: []
-  }
-  async componentDidMount() {
-    const categoryList = await API.getMovies()
-    store.dispatch({
-      type: 'SET_CATEGORY_LIST',
-      payload: {
-        categoryList
-      }
-    })
-    const suggestionList = await API.getSuggestion(10)
-    store.dispatch({
-      type: 'SET_SUGGESTION_LIST',
-      payload: {
-        suggestionList
-      }
-    })
-    // console.log(movies)
-    // console.log(categories)
-    // this.setState({
-    //   suggestionList: movies,
-    //   categoryList: categories
-    // })
-  }
   render() {
     return (
       <Provider
@@ -51,13 +16,7 @@ export default class App extends Component<Props> {
           loading={<Loading />}
           persistor={persistor}
         >
-          <Home>
-            <Header />
-            <Player />
-            <Text>Buscador</Text>
-            <CategoryList />
-            <SuggestionList />
-          </Home>
+          <AppLayout />
         </PersistGate>
       </Provider>
     );
